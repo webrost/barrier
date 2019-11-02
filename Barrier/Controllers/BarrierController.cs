2dcd212a7179b36189692f27a080eda095cbbfc2
rostislav <rostislav.anisimov@gmail.com>
2019-11-02 20:53:21 +0200

    Barrir start by Rost

---
+++ Barrier/Controllers/BarrierController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Barrier.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BarrierController : ControllerBase
+    {
+        private Models.BarrierContext db;
+
+        public BarrierController(Models.BarrierContext context)
+        {
+            db = context;
+        }
+
+        [HttpGet("houses")]
+        public string Houses() {
+            return db.Houses.First().Name;
+        }
+    }
+}/
---
+++ Barrier/Models/BarrierContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Barrier.Models
+{
+    public class BarrierContext : DbContext
+    {
+        public DbSet<House> Houses { get; set; }
+        public BarrierContext(DbContextOptions<BarrierContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
